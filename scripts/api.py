b1d50c8f3ad562238eddfacfd911833b62e9c7ed
Varsha Parthasarathy <varshap@lyft.com>
2020-03-09 15:57:14 -0700

    first push

---
+++ scripts/api.py
@@ -0,0 +1,38 @@
+import sys
+
+from kubernetes import client, config
+import logging
+import os
+logger = logging.getLogger()
+
+def _k8s_setup():
+    print "in K8 setup"
+    contexts, active_context = config.list_kube_config_contexts()
+    print contexts
+    print active_context
+    if not contexts:
+        logger.error("Cannot find any context in kubeconfig file.")
+        sys.exit(0)
+    logger.info("found and read existing kube context")
+    logger.info("interacting with cluster {}".format(active_context['context']['cluster']))
+    print active_context['context']['cluster']
+    print(os.environ['KUBECONFIG'])
+    kubeconfig =  os.getenv('KUBECONFIG')
+
+    config.load_kube_config(kubeconfig)
+
+    v1 = client.CoreV1Api()
+
+    list_of_all_nodes = v1.list_node()
+    for node_map in list_of_all_nodes.items:
+        node_id = node_map.metadata.name
+        print("----------", node_id)
+        # now describe this node.
+        node = v1.read_node(name=node_id)
+        #print("## ", node)
+        pod = v1.list_namespaced_pod(node)
+        print("podddd ", pod)
+
+
+if __name__ == "__main__":
+    _k8s_setup()
